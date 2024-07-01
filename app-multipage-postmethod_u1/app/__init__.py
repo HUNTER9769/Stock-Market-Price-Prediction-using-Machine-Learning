5710c5a474242d340de1b7c0ca8c127420ef982b
Pratim Bhagat <bhagatpratim21@gmail.com>
2024-07-01 11:50:14 +0530

    removed unwanted files

--- app-multipage-postmethod_u1/app/__init__.py
+++
@@ -1,4 +0,0 @@
-from .server import create_app
-# from .graph import layout
-# from .signup import layout
-# from .signin import layout
--- app/run.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-# Run the app
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
